ef64a94d9b223ee977d89e467c0ba401309c812c
23mhmq100 <147487977+23mhmq100@users.noreply.github.com>
2023-12-31 11:23:47 +0530

    #include<stdio.h> int a(int n) {     int sum=0;     for(int i=1;i<=n/2;i++)     {         if(n%i==0)         {             sum+=i;         }     }     if(sum>n)     {         return 1;     }     else     {         return 0;     } } int main() {     int n;     scanf("%d",&n);     if(a(n))     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int a(int n)
+{
+    int sum=0;
+    for(int i=1;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            sum+=i;
+        }
+    }
+    if(sum>n)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(a(n))
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
